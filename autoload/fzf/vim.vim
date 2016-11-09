junegunn/fzf.vim

commit 6a4760b87e1dffdb7797b06aa4b2999cca9aecb1

Use fugitive (if available) to get the git root (#239)

autoload/fzf/vim.vim
@@ -403,8 +403,17 @@ endfunction
 " GFiles[?]
 " ------------------------------------------------------------------
 
+" helper function to get the git root. Uses vim-fugitive if available for EXTRA SPEED!
+function! s:get_git_root()
+  try
+    return fugitive#repo().tree()
+  catch
+    return split(system('git rev-parse --show-toplevel'), '\n')[0]
+  endtry
+endfunction
+
 function! fzf#vim#gitfiles(args, ...)
-  let root = split(system('git rev-parse --show-toplevel'), '\n')[0]
+  let root = s:get_git_root()
   if v:shell_error
     return s:warn('Not in git repo')
   endif
@@ -938,7 +947,7 @@ function! s:commits_sink(lines)
 endfunction
 
 function! s:commits(buffer_local, args)
-  let s:git_root = s:chomp(system('git rev-parse --show-toplevel'))
+  let s:git_root = s:get_git_root()
   if v:shell_error
     return s:warn('Not in git repository')
   endif

commit 7721fb2677e601943c9d6872ae36380cac66b78c

Check if fugitive#repo() exists
Caused by #239. Close #241.

autoload/fzf/vim.vim
@@ -405,12 +405,14 @@ endfunction
 
 " helper function to get the git root. Uses vim-fugitive if available for EXTRA SPEED!
 function! s:get_git_root()
-  try
-    return fugitive#repo().tree()
-  catch
-    let root = split(system('git rev-parse --show-toplevel'), '\n')[0]
-    return v:shell_error ? '' : root
-  endtry
+  if exists('*fugitive#repo')
+    try
+      return fugitive#repo().tree()
+    catch
+    endtry
+  endif
+  let root = split(system('git rev-parse --show-toplevel'), '\n')[0]
+  return v:shell_error ? '' : root
 endfunction
 
 function! fzf#vim#gitfiles(args, ...)